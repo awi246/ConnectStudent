a24c08cc78b855a1aced6afe7313cf23c031b626
Awiral Chand <103342572+awi246@users.noreply.github.com>
2023-12-07 07:35:46 +0545

    rever:comment

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -8,7 +8,7 @@ function Box() {
       <Avatar  />
     </div>
     <div className="Box__question">
-      {/* <h5>What is your question or link?</h5> */}
+      <h5>What is your question or link?</h5>
     </div>
     <h1>heyy checking again</h1>
   </div>
