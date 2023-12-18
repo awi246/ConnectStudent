9eed98a86c8dcb225face3d6f3e5836af2c34943
Manoj Khatri <iamrmnj@gmail.com>
2023-12-18 23:17:07 +0545

    feat:changed box text

--- frontend/src/components/UI/Box/box.jsx
+++ frontend/src/components/UI/Box/box.jsx
@@ -4,13 +4,14 @@ import "../../../styles/Box.css";
 
 function QuoraBox() {
   const user = useSelector(selectUser);
+  console.log("user",user);
   return (
     <div className="Box transform cursor-pointer hover:bg-blue-gray-50 shadow-sm hover:shadow-lg hover:scale-105 transition-transform duration-300">
       <div className="Box__info">
         <img src={user?.photo}  className="rounded-full shadow-sm m-auto"/>
       </div>
-      <div className="Box__quora text-center">
-        <h5 className="font-medium">What is your question or link?</h5>
+      <div className="Box__quora text-center pt-2">
+        <h5 className="font-medium">Hello <span className="text-red-300">{user?.userName}</span>, {' '}Add your question or go through the answer effortlessly!!!</h5>
       </div>
     </div>
   );
