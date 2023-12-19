6ef8da26a27828a27fcc938208e53a9f784486ec
Manoj Khatri <iamrmnj@gmail.com>
2023-12-19 17:01:05 +0545

    fix:typo error

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="fixed bottom-0 left-0 w-full text-white text-center bg-red-500/90 max-h-[100px]">
       <div className="flex flex-row justify-center items-center -mb-6">
-        <p>&copy; {currentYear} Connect Student™</p>
+        <p>&copy; {currentYear} Connect Students™</p>
         <img src={Logo} />
         All Rights Reserved.
       </div>
