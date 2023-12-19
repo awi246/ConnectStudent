eec744949b37935747f75ab87a83876a3fe69f5a
Manoj Khatri <iamrmnj@gmail.com>
2023-12-19 16:13:57 +0545

    fix:footer

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -1,10 +1,14 @@
+import Logo from "../../assets/newLogo.svg";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="fixed bottom-0 left-0 w-full text-white text-center p-[10px] bg-red-500/90">
-      <p>&copy; {currentYear} Connect Students</p>
-      <span>For CSIT, By CSIT</span>
+    <div className="fixed bottom-0 left-0 w-full text-white text-center bg-red-500/90 max-h-[90px]">
+      <div className="flex flex-row justify-center items-center -mb-6">
+        <p>&copy; {currentYear} Connect Students</p>
+        <img src={Logo} />
+      </div>
+      <span className="">For CSIT, By CSIT</span>
     </div>
   );
 };
