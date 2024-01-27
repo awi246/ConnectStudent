243a243c47a37496cc6f904997329b16497616aa
Manoj Khatri <iamrmnj@gmail.com>
2024-01-27 21:25:48 +0545

    feat: teacher notification

--- frontend/src/components/UI/Box/box.jsx
+++ frontend/src/components/UI/Box/box.jsx
@@ -7,10 +7,27 @@ function QuoraBox() {
   return (
     <div className="Box transform cursor-pointer hover:bg-blue-gray-50 shadow-sm hover:shadow-lg hover:scale-105 transition-transform duration-300">
       <div className="Box__info">
-        <img src={user?.photo}  className="rounded-full shadow-sm m-auto"/>
+        <img src={user?.photo} className="rounded-full shadow-sm m-auto" />
       </div>
       <div className="Box__quora text-center pt-2">
-        <h5 className="font-medium">Hello <span className="text-red-500">{user?.userName ? user?.userName : 'Teacher'}</span>, {' '}Add your question or go through the answer effortlessly!!!</h5>
+        {user?.role === "Admin" ? (
+          <h5 className="font-medium">
+            Hello{" "}
+            <span className="text-red-500">
+              {user?.userName ? user?.userName : "Teacher"}
+            </span>
+            , You have been appointed as the moderator of this website. Please
+            moderate the content accordingly and improve the user experience.
+          </h5>
+        ) : (
+          <h5 className="font-medium">
+            Hello{" "}
+            <span className="text-red-500">
+              {user?.userName ? user?.userName : "Teacher"}
+            </span>
+            , Add your question or go through the answer effortlessly!!!
+          </h5>
+        )}
       </div>
     </div>
   );
--- frontend/src/layouts/RightSide/index.jsx
+++ frontend/src/layouts/RightSide/index.jsx
@@ -3,20 +3,26 @@ import Notification from "./Notifications.jsx";
 import "../../styles/Widget.css";
 import { IoHeart } from "react-icons/io5";
 import notification from "../../assets/bell3d.png";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../feature/userSlice";
+
 function Widget() {
+  const user = useSelector(selectUser);
   return (
     <div className="w-[380px]">
-      <div className="widget mb-8 w-full max-h-[400px] overflow-y-auto">
-        <div className="widget__header">
-          <h5 className="flex flex-row items-center gap-1 font-semibold">
-            <span className="text-xl">Notifications</span>
-            <img src={notification} width={20}/>
-          </h5>
+      {(user?.type === "teacher" || user?.role === "Admin") && (
+        <div className="widget mb-8 w-full max-h-[400px] overflow-y-auto">
+          <div className="widget__header">
+            <h5 className="flex flex-row items-center gap-1 font-semibold">
+              <span className="text-xl">Notifications</span>
+              <img src={notification} width={20} />
+            </h5>
+          </div>
+          <div className="widget__contents">
+            <Notification />
+          </div>
         </div>
-        <div className="widget__contents">
-          <Notification />
-        </div>
-      </div>
+      )}
       <div className="widget w-full">
         <div className="widget__header">
           <h5 className="flex flex-row items-center gap-1">
